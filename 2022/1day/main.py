03013ad76968d1c814f4c329ba25990ce503431d
useless_pony <ramkeykill@gmail.com>
2022-12-04 12:16:02 -0500

    cleaned up day 1

--- 2022/1day/main.py
+++ 2022/1day/main.py
@@ -20,34 +20,34 @@ input = """1000
 with open("part1.input") as i:
         input=i.read()
 
-def structurize(n):
-	a=[]
-	for i in n[:-1].split("\n\n"):
-		a.append([[int(q) for q in i.split("\n")]])
-	return(a)
+# [[[calories, ...], <reserved for sum>], ...]
+def structurize(serialdata):
+	data=[]
+	for group in serialdata[:-1].split("\n\n"):
+		data.append([[int(line) for line in group.split("\n")]])
+	return(data)
 
 s=structurize(input)
-#print(s)
+# add the sums into the structurized data
+for index, item in enumerate(s):
+        s[index].append(sum(item[0]))
 
-for n,i in enumerate(s):
-        s[n].append(sum(i[0]))
+#find the top #a of the structurized data
+def top(number):
+        output=[]
+        for _ in range(number):
+                tmp=["",0]
+                for index, item in enumerate(s):
+                        if item[1] > tmp[1] :
+                                tmp = [index, item[1]]
+                if tmp[0]: output.append(s.pop(tmp[0]))
+        return output
 
-#print(s)
-
-def top(a):
-        o=[]
-        for e in range(a):
-                q=["",0]
-                for n,i in enumerate(s):
-                        if i[1] > q[1] :
-                                q = [n,i[1]]
-                if q[0]: o.append(s.pop(q[0]))
-        return o
-
+#print top 3 so part1 can still be done without duplicat code.
 top = top(3)
-
 print(top)
 
+#sum the top3 and print
 a=0
 for i in top:
         a += i[1]
