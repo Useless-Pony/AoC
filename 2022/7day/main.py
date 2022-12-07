f9c01d119dac2ed9e4819fa9d69035d15497ca2d
useless_pony <ramkeykill@gmail.com>
2022-12-06 19:20:55 -0500

    setup for day 7

---
+++ 2022/7day/main.py
@@ -0,0 +1,11 @@
+#example input used to test basics
+inputs =[""]
+
+#with open("part1.input") as i:
+#        inputs.append(i.read()[:-1])
+
+for input in inputs:
+	print("part1")
+
+	print("part2")
+print("done"):
