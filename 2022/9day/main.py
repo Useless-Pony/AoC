5bdc24461d7f321fb9112a287dbca511186160d6
useless_pony <ramkeykill@gmail.com>
2022-12-10 17:02:06 -0500

    day 9 part 2 is scuffed as fluff

--- 2022/9day/main.py
+++ 2022/9day/main.py
@@ -44,5 +44,42 @@ for input in inputs:
 	print(sum([sum(row) for row in board]))
 
 	print("##part2")
+## this is all scuffed
+	SIZE=5
+	LEN=2
+	board = [[0 for item in range(SIZE)] for row in range(SIZE)]
+	board[0][0] = 1
+	X, Y = 0, 1
+	moves = {"U":[1, 0], "D":[1, 0], "L":[0, -1], "R":[0, 1]}
+	offsets = {
+		"[2, 0]":[1, 0], "[-2, 0]":[1, 0], "[0, -2]":[0, -1], "[0, 2]":[0, 1],
+		"[2, 1]":[1, 1], "[-2, 1]":[1, 1], "[1, -2]":[1, -1], "[1, 2]":[1, 1],
+		"[2, -1]":[1, -1], "[-2, -1]":[1, -1], "[-1, -2]":[-1, -1], "[-1, 2]":[-1, 1]}
+	rope = [[0, 0] for i in range(LEN)]
+	for line in input.splitlines():
+		offset = moves[line[0]]
+		move = int(line.split()[1])
+		for i in range(move):
+#try to not have a special case
+#makes first "prev" a ghost knot that pulls the head to where it's supposed to move
+			prev_knot = rope[0].copy()
+			prev_knot = [sum(itm) for itm in list(zip(prev_knot, offset, offset))]
+			prev_pos = []
+			for ind,knot in enumerate(rope):
+				knot_tmp = knot.copy()
+
+				rel = [prev - cur for prev,cur in zip(prev_knot, knot)]
+				print(rel)
+				if str(rel) in offsets:
+					knot = [knot[X]+offsets[str(rel)][X],
+						knot[Y]+offsets[str(rel)][Y]
+						]
+				print(knot)
+
+				prev_knot = knot
+				prev_pos = knot_tmp
+			board[rope[LEN-1][X]][rope[LEN-1][Y]] = 1
+			print(*board[::-1], sep="\n", end="\n\n")
+	print(sum([sum(row) for row in board]))
 
 print("done")
