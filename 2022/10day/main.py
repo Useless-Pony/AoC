751aba8fcadde0b6cfb2c38a5036e39c26ab203c
useless_pony <ramkeykill@gmail.com>
2022-12-10 18:15:34 -0500

    compleated day 10

---
+++ 2022/10day/main.py
@@ -0,0 +1,166 @@
+#example input used to test basics
+inputs =["""
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+"""[1:-1]]
+
+with open("part1.input") as i:
+        inputs.append(i.read()[:-1])
+
+
+for input in inputs:
+	X = [1,1]
+	for line in input.splitlines():
+		X.append(X[-1])
+		if line[0] == "a":
+			X.append(X[-1] + int(line.split()[-1]))
+	print("##part1")
+	print(sum([X[ind] * ind for ind in range(20,len(X),40)]))
+	print("##part2")
+	for cycle,value in enumerate(X):
+		print("#" if value <= (cycle%40) <= (value+2) else ".", end="" if cycle % 40 else "\n")
+print("done")
